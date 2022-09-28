5bf4d12921c4ae05cb39e595dc0d5ecc11d2af12
Kota Tohata <koutapro1@gmail.com>
2022-09-28 10:42:27 +0900

    add: seedファイルを作成しモルフのデータを追加

--- back/db/seeds.rb
+++ back/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Morph.create(name: 'White Out', symbol: 'W')
+Morph.create(name: 'Oreo', symbol: 'O')
+Morph.create(name: 'Zulu', symbol: 'Z')
+Morph.create(name: 'Ghost', symbol: 'G')
+Morph.create(name: 'Patternless', symbol: 'P')
+Morph.create(name: 'Caramel Albino', symbol: 'C')
+Morph.create(name: 'Amel Albino', symbol: 'A')
