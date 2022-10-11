50182d72a02b54b518c19dd986f14cdc413356f9
Kota Tohata <koutapro1@gmail.com>
2022-10-11 14:41:39 +0900

    add: スタイルを調整

--- front/pages/calculator.vue
+++ front/pages/calculator.vue
@@ -28,74 +28,68 @@
             (as you may have HETs that you are not aware of and haven't added them to the calculation).
           </p>
         </div>
-        <div class="search-form-container">
-          <div class="search-form">
-            <v-row justify="space-around" align-content="center" class="select-row">
-              <v-col cols="6" sm="5" class="male-search-form">
-                <h2>♂ Male</h2>
-                <div class="male-select-container">
-                  <v-autocomplete
-                    class="male-visual-select"
-                    v-model="male_visual"
-                    :items="morphs"
-                    label="male visual"
-                    item-value="symbol"
-                    item-text="name"
-                    color="white"
-                    chips
-                    deletable-chips
-                    multiple
-                  ></v-autocomplete>
-                  <v-autocomplete
-                    v-model="male_het"
-                    :items="morphs"
-                    label="male het"
-                    item-value="symbol"
-                    item-text="name"
-                    color="white"
-                    chips
-                    deletable-chips
-                    multiple
-                  ></v-autocomplete>
-                </div>
-              </v-col>
-              <v-col cols="6" sm="5" class="female-search-form">
-                <h2>♀ Female</h2>
-                <div class="female-select-container">
-                  <v-autocomplete
-                    v-model="female_visual"
-                    :items="morphs"
-                    label="female visual"
-                    item-value="symbol"
-                    item-text="name"
-                    color="white"
-                    chips
-                    deletable-chips
-                    multiple
-                  ></v-autocomplete>
-                  <v-autocomplete
-                    v-model="female_het"
-                    :items="morphs"
-                    label="female het"
-                    item-value="symbol"
-                    item-text="name"
-                    color="white"
-                    chips
-                    deletable-chips
-                    multiple
-                  ></v-autocomplete>
-                </div>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col class="egg-image" justify="center">
-                <img src="~assets/images/stems/egg.png">
-                <div class="button-area">
-                  <button @click="moveToResultPage" class="ok-button">OK</button>
-                </div>
-              </v-col>
-            </v-row>
-          </div>
+        <div class="search-form">
+          <v-row justify="space-around" align-content="center" class="select-area">
+            <v-col cols="6" sm="5" class="male-search-form">
+              <h2>♂ Male</h2>
+              <div class="male-select-container">
+                <v-autocomplete
+                  class="male-visual-select"
+                  v-model="male_visual"
+                  :items="morphs"
+                  label="male visual"
+                  item-value="symbol"
+                  item-text="name"
+                  color="white"
+                  chips
+                  deletable-chips
+                  multiple
+                ></v-autocomplete>
+                <v-autocomplete
+                  v-model="male_het"
+                  :items="morphs"
+                  label="male het"
+                  item-value="symbol"
+                  item-text="name"
+                  color="white"
+                  chips
+                  deletable-chips
+                  multiple
+                ></v-autocomplete>
+              </div>
+            </v-col>
+            <v-col cols="6" sm="5" class="female-search-form">
+              <h2>♀ Female</h2>
+              <div class="female-select-container">
+                <v-autocomplete
+                  v-model="female_visual"
+                  :items="morphs"
+                  label="female visual"
+                  item-value="symbol"
+                  item-text="name"
+                  color="white"
+                  chips
+                  deletable-chips
+                  multiple
+                ></v-autocomplete>
+                <v-autocomplete
+                  v-model="female_het"
+                  :items="morphs"
+                  label="female het"
+                  item-value="symbol"
+                  item-text="name"
+                  color="white"
+                  chips
+                  deletable-chips
+                  multiple
+                ></v-autocomplete>
+              </div>
+            </v-col>
+          </v-row>
+        </div>
+        <div class="submit-area">
+          <img src="~assets/images/stems/egg.png" class="egg-image">
+            <button @click="moveToResultPage" class="ok-button">OK</button>
         </div>
       </v-container>
     </v-main>
@@ -157,11 +151,6 @@
     font-weight: bold;
     line-height: 1;
   }
-  .search-form-container
-  {
-    display: flex;
-    justify-content: center;
-  }
   .search-form
   {
     position: relative;
@@ -178,6 +167,11 @@
     font-size: 2rem;
     line-height: 1.2;
   }
+  .select-area
+  {
+    width: 90%;
+    margin: auto;
+  }
   .male-search-form, .female-search-form
   {
     background-color: rgb(179,135,88);
@@ -189,28 +183,28 @@
   {
     padding: 2rem;
   }
+  .submit-area
+  {
+    position: relative;
+    top: -5rem;
+    z-index: 20;
+    pointer-events: none;
+  }
+  /* .submit-area :not(button)
+  {
+    pointer-events: none;
+  } */
   .egg-image
   {
-    display: flex;
-    justify-content: center;
-    position: absolute;
-    top: 13rem;
-    z-index: 11;
-  }
-  .egg-image img
-  {
-    /* z-index: 100; */
-  }
-  .button-area
-  {
-    position: absolute;
-    bottom: 1rem;
-    left: 50%;
-    transform: translateX(-50%);
-    z-index: 110;
+    display: block;
+    margin: auto;
   }
   .ok-button
   {
+    position: relative;
+    top: -5rem;
+    display: block;
+    margin: auto;
     background-color: rgb(199,120,90);
     color: rgb(248,255,255);
     border: solid 8px rgb(248,255,255);
@@ -221,6 +215,7 @@
     width: 10rem;
     height: 5rem;
     box-shadow: 2px 4px 8px rgba(0, 0, 0, 0.7);
+    pointer-events: auto;
     transition: .3s;
   }
   .ok-button:hover
