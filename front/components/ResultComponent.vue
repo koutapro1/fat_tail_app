b6e288c09068232a00149cf390d54d50ec4d2a99
Kota Tohata <koutapro1@gmail.com>
2022-11-02 20:43:24 +0900

    add: フォーム欄と結果欄をコンポーネントに切り出し

---
+++ front/components/ResultComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container fluid class="main-container">
+        <top-images-component />
+        <div v-for="result in results" class="d-flex">
+          <div v-if="result.visual.length > 0" class="visual">
+            <span v-for="visual in result.visual">
+              {{visual}}
+            </span>
+          </div>
+          <div v-else class="visual">
+            <span>
+              Normal
+            </span>
+          </div>
+          <div v-if="result.het.length > 0" class="het pl-2 pr-5">
+            het
+            <span v-for="het in result.het">
+              {{het}}
+            </span>
+          </div>
+          <div class="possibility">
+            {{result.possibility}}%
+          </div>
+        </div>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    props: [
+      'results'
+    ],
+  }
+</script>
--- front/pages/calculator.vue
+++ front/pages/calculator.vue
@@ -3,23 +3,6 @@
     <v-main>
       <v-container fluid class="main-container">
         <top-images-component />
-        <!-- <div class="top-images">
-          <div class="left-leaf">
-            <img src="~assets/images/stems/leaf1.png">
-          </div>
-          <div class="right-leaf">
-            <img src="~assets/images/stems/leaf2.png">
-          </div>
-          <div class="title">
-            <img src="~assets/images/stems/title.png">
-          </div>
-          <div class="left-shadow">
-            <img src="~assets/images/stems/left-shadow.png">
-          </div>
-          <div class="right-shadow">
-            <img src="~assets/images/stems/right-shadow.png">
-          </div>
-        </div> -->
         <div class="explanation">
           <p>
             All you need to do now is choose the genetics of your pairing below.<br>
@@ -29,69 +12,17 @@
             (as you may have HETs that you are not aware of and haven't added them to the calculation).
           </p>
         </div>
-        <div class="search-form">
-          <v-row justify="space-around" align-content="center" class="select-area">
-            <v-col cols="6" sm="5" class="male-search-form">
-              <h2>♂ Male</h2>
-              <div class="male-select-container">
-                <v-autocomplete
-                  class="male-visual-select"
-                  v-model="male_visual"
-                  :items="morphs"
-                  label="male visual"
-                  item-value="symbol"
-                  item-text="name"
-                  color="white"
-                  chips
-                  deletable-chips
-                  multiple
-                ></v-autocomplete>
-                <v-autocomplete
-                  v-model="male_het"
-                  :items="morphs"
-                  label="male het"
-                  item-value="symbol"
-                  item-text="name"
-                  color="white"
-                  chips
-                  deletable-chips
-                  multiple
-                ></v-autocomplete>
-              </div>
-            </v-col>
-            <v-col cols="6" sm="5" class="female-search-form">
-              <h2>♀ Female</h2>
-              <div class="female-select-container">
-                <v-autocomplete
-                  v-model="female_visual"
-                  :items="morphs"
-                  label="female visual"
-                  item-value="symbol"
-                  item-text="name"
-                  color="white"
-                  chips
-                  deletable-chips
-                  multiple
-                ></v-autocomplete>
-                <v-autocomplete
-                  v-model="female_het"
-                  :items="morphs"
-                  label="female het"
-                  item-value="symbol"
-                  item-text="name"
-                  color="white"
-                  chips
-                  deletable-chips
-                  multiple
-                ></v-autocomplete>
-              </div>
-            </v-col>
-          </v-row>
-        </div>
-        <div class="submit-area">
-          <img src="~assets/images/stems/egg.png" class="egg-image">
-            <button @click="moveToResultPage" class="ok-button">OK</button>
-        </div>
+        <CalculatorComponent
+          v-if="isVisibleCalculator"
+          :selected_morphs="selected_morphs"
+          :morphs="morphs"
+          @search-result="searchResult"
+          @set-selected-morphs="setSelectedMorphs"
+        />
+        <ResultComponent
+          v-if="isVisibleResult"
+          :results="results"
+        />
       </v-container>
     </v-main>
   </v-app>
@@ -238,16 +169,22 @@
 
 <script>
   import { mapGetters, mapActions } from 'vuex'
+  import axios from "~/plugins/axios"
   import anime from 'animejs'
 
   export default {
     layout: 'plain',
     data() {
       return {
-        male_visual: [],
-        male_het: [],
-        female_visual: [],
-        female_het: [],
+        selected_morphs: {
+          male_visual: [],
+          male_het: [],
+          female_visual: [],
+          female_het: [],
+        },
+        results: [],
+        isVisibleCalculator: true,
+        isVisibleResult: false,
       }
     },
     computed: {
@@ -261,16 +198,28 @@
     },
     methods: {
       ...mapActions(["fetchMorphs"]),
-      moveToResultPage() {
-        this.$router.push({
-          path: "/result",
-          query: {
-            male_visual: this.male_visual,
-            male_het: this.male_het,
-            female_visual: this.female_visual,
-            female_het: this.female_het
+      setSelectedMorphs(newVal) {
+        this.selected_morphs = newVal;
+      },
+      handleShowResult() {
+        this.isVisibleResult = true;
+        this.isVisibleCalculator = false;
+      },
+      searchResult() {
+        axios.get("/morphs/calculate",
+        {
+          params: {
+            male_visual: this.selected_morphs.male_visual,
+            male_het: this.selected_morphs.male_het,
+            female_visual: this.selected_morphs.female_visual,
+            female_het: this.selected_morphs.female_het
           }
-        })
+        }).then(res => {
+          if (res.data) {
+            this.results = res.data
+          }
+        });
+        this.handleShowResult();
       },
       setAnimation() {
         const animation = anime.timeline({});
